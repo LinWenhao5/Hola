71b0819b5a362ff829a08c51a194db386f9141ac
Lin Wenhao <linwenhao5@gmail.com>
2022-09-07 13:46:27 +0200

    ...

--- index.php
+++ index.php
@@ -8,9 +8,6 @@
     <link rel="stylesheet" href="index.css">
 </head>
 <body>
-    
-</body>
-
 <?php
     $quiz = [
         [
@@ -41,10 +38,12 @@
     ];
 
     $goed = 0;
+    ?>
 
+<div class="form-home">
+<?php
     for ($i = 0; $i < count($quiz); $i++) {
     ?>
-    <div class="form-home">
         <form method="post" action="index.php">
             <label for="vraag"><?php echo $quiz[$i]["vraag"] ?></label><br>
             <input type="radio" name="<?php echo $quiz[$i]["id"] ?>" value="<?php echo $quiz[$i]["antwoord_een"] ?>">
@@ -55,10 +54,11 @@
             <label for="antwoord_drie"><?php echo $quiz[$i]["antwoord_drie"] ?></label><br><br>
     <?php
     }
-?>
-     <div class="button-home"><input type="submit" value="verzenden"></div> 
+    ?>
+            <div class="button-home"><input type="submit" value="verzenden"></div>
         </form>
 </div>
+
 <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         for ($i = 0; $i < count($quiz); $i++) {
@@ -67,9 +67,10 @@
             }
         }
         $score = $goed / count($quiz) * 100;
-        echo "Je had " . count($quiz) . " van de " . $goed . " vragen goed.<br>";
+        echo "Je had " . $goed . " van de " . count($quiz) . " vragen goed.<br>";
         echo "De score is " . $score . "%";
     }
 
 ?>
+</body>
 </html>
