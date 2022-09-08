0b972fede6d8d8a77f359efd4838df6200f93e60
Hamza el Amri <hamzaelamri2003@gmail.com>
2022-09-08 11:56:16 +0200

    e

---
+++ website/project/quiz-html.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="quiz.css">
+</head>
+<body>
+<?php
+    $quiz = [
+        [
+            "vraag" => "Maak de tekst dik gedrukt bij degradation<br>
+            <p><br>
+            WWF's mission is to stop the []degradation[] of our planet's natural environment.<br>
+            </p>",
+            "antwoord_een" => "strong",
+            "antwoord_twee" => "br",
+            "antwoord_drie" => "em",
+            "goede_antwoord" => "strong",
+            "id" => 0
+        ],
+        [
+            "vraag" => "<form action=/action_page.php >[]   
+            Name: <input type=text name=name>
+            <input type=submit>
+            </form>",
+            "antwoord_een" => "method=get",
+            "antwoord_twee" => "method=push",
+            "antwoord_drie" => "method=post",
+            "goede_antwoord" => "method=post",
+            "id" => 1
+        ],
+        [
+            "vraag" => "<body><br>
+            <p id=demo>Hi.</p>
+            <script><br>
+            document.[](demo).innerHTML = Hello World!;<br>
+            </script><br>
+            </body>",
+            "antwoord_een" => "id",
+            "antwoord_twee" => "getelement",
+            "antwoord_drie" => "getElementById",
+            "goede_antwoord" => "getElementById",
+            "id" => 2
+        ]
+    ];
+
+    $goed = 0;
+    ?>
+
+<div class="form-home">
+    <form method="post" action="quiz.php">
+        <label for="title"><h2>Wat hoort tussen de []!</h2></label>
+<?php
+    for ($i = 0; $i < count($quiz); $i++) {
+    ?>
+            <label for="vraag"><?php echo $quiz[$i]["vraag"] ?></label><br>
+            <input type="radio" name="<?php echo $quiz[$i]["id"] ?>" value="<?php echo $quiz[$i]["antwoord_een"] ?>">
+            <label for="antwoord_een"><?php echo $quiz[$i]["antwoord_een"] ?></label><br>
+            <input type="radio" name="<?php echo $quiz[$i]["id"] ?>" value="<?php echo $quiz[$i]["antwoord_twee"] ?>">
+            <label for="antwoord_twee"><?php echo $quiz[$i]["antwoord_twee"] ?></label><br>
+            <input type="radio" name="<?php echo $quiz[$i]["id"] ?>" value="<?php echo $quiz[$i]["antwoord_drie"] ?>">
+            <label for="antwoord_drie"><?php echo $quiz[$i]["antwoord_drie"] ?></label><br><br>
+    <?php
+    }
+    ?>
+            <input type="submit" value="verzenden">
+        </form>
+
+<?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST)) {
+        for ($i = 0; $i < count($quiz); $i++) {
+            if ($_POST[$i] == $quiz[$i]["goede_antwoord"]) {
+                $goed += 1;
+            } else {
+                $id = $i + 1;
+                echo "<p>Vraag {$id}: Uw antwoord is <a class='red'>{$_POST[$i]}</a> en het juiste antwoord is <a class='green'>{$quiz[$i]["goede_antwoord"]}</a>.</p>";
+            }
+        }
+        $score = $goed / count($quiz) * 100;
+        $score = round($score);
+        echo "Je had " . $goed . " van de " . count($quiz) . " vragen goed.<br>";
+        echo "De score is " . $score . "%<br>";
+        ?>
+        <a href="https://www.w3schools.com/tags/default.asp">Hier voor extra uitleg</a>
+        <?php
+    }
+?>
+</div>
+</body>
+</html>
